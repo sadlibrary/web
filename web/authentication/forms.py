66e07cafb7b55eb356688f8df5f83fdd0b23bc52
Nazanin Azarian <badi.mojgan@gmail.com>
2022-08-06 01:29:51 +0430

    Fix merge issues

--- web/authentication/forms.py
+++ web/authentication/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class LoginForm(AuthenticationForm):
@@ -25,3 +25,27 @@ class LoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password', 'remember_me']
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100,
+                                 required=True,
+                                 widget=forms.TextInput())
+    last_name = forms.CharField(max_length=100,
+                                required=True,
+                                widget=forms.TextInput())
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput())
+    email = forms.EmailField(required=True,
+                             widget=forms.TextInput())
+    password1 = forms.CharField(max_length=50,
+                                required=True,
+                                widget=forms.PasswordInput(attrs={'id': 'password',}))
+    password2 = forms.CharField(max_length=50,
+                                required=True,
+                                widget=forms.PasswordInput(attrs={'id': 'password',}))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
