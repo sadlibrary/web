45b593cabb97fb094158e9316e9ee476c9f048bd
rerfan24 <rezaerfan26@gmail.com>
2022-08-07 18:06:30 +0430

    fix render login/register

--- web/authentication/static/authentication/script.js
+++ web/authentication/static/authentication/script.js
@@ -1,11 +1,23 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+var renderType = document.getElementById("typeVar").value;
+
+if (renderType == "register") {
+  try {
+    container.classList.add("right-panel-active");
+  } catch (e) {}
+} else {
+  try {
+    container.classList.remove("right-panel-active");
+  } catch (e) {}
+}
+
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
 });
