33e0114a53566fb7a16b9ef86cdbadf3d5af839f
rerfan24 <rezaerfan26@gmail.com>
2022-08-07 19:28:20 +0430

    fix render login/register

--- web/authentication/static/authentication/script.js
+++ web/authentication/static/authentication/script.js
@@ -2,18 +2,6 @@ const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
 const container = document.getElementById("container");
 
-var renderType = document.getElementById("typeVar").value;
-
-if (renderType == "register") {
-  try {
-    container.classList.add("right-panel-active");
-  } catch (e) {}
-} else {
-  try {
-    container.classList.remove("right-panel-active");
-  } catch (e) {}
-}
-
 signUpButton.addEventListener("click", () => {
   container.classList.add("right-panel-active");
 });
