2db4185098a4cd90557400a20a75b789a04a1c5b
Arshan <arshandalili@gmail.com>
2022-08-22 22:42:06 +0430

    Fixed error

--- web/library/forms.py
+++ web/library/forms.py
@@ -4,7 +4,7 @@ from library.models import *
 class TypeForm(forms.ModelForm):
     name = models.CharField(max_length=256)
     formats = models.TextField()
-    accepted_attachments_ids = models.TextField
+    accepted_attachments_ids = models.TextField()
     
     class Meta:
         model = Library
