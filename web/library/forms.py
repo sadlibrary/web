d665c631cbd9b112375ea1e4e3f16773d956d1c7
Arshan <arshandalili@gmail.com>
2022-08-22 22:41:35 +0430

    Added TypeForm

---
+++ web/library/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from library.models import *
+
+class TypeForm(forms.ModelForm):
+    name = models.CharField(max_length=256)
+    formats = models.TextField()
+    accepted_attachments_ids = models.TextField
+    
+    class Meta:
+        model = Library
+        fields = ['name', 'formats', 'accepted_attachments_ids']
