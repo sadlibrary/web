c2061d8f3bc9963ce99ddebbc891b384c7cc2c9a
Pouya Esmaili <poesmaili@gmail.com>
2022-08-23 00:37:18 +0430

    Fix profile link

--- web/authentication/urls.py
+++ web/authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from authentication import views
 
+
+app_name = 'authentication'
 urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.auth_logout, name='logout'),
