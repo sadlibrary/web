08f321cfb10a624ce8a8b089026985614478e92e
Soroush Jahanzad <SoroushJahanzad@gmail.com>
2022-08-30 00:50:59 +0430

    Add media routing

--- web/web/urls.py
+++ web/web/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import authentication
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('library/', include('library.urls')),
     path('', authentication.views.start_page)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
